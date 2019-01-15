437fd9c865bd23aca74ada9c21717cc0483621f1
mrome007 <mrome007@ucr.edu>
2019-01-14 19:00:15 -0800

    Initial pocket monsters commit. Mimic pokemon gameplay.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 0.2f;
+    private Vector2 movementVector;
+    private Rigidbody2D playerRigidBody2d;
+
+    private void Awake()
+    {
+        playerRigidBody2d = GetComponent<Rigidbody2D>();
+        movementVector = Vector2.zero;
+    }
+
+    private void FixedUpdate()
+    {
+        var h = Input.GetAxis("Horizontal");
+        var v = Input.GetAxis("Vertical");
+
+        movementVector.x = h;
+        movementVector.y = v;
+
+        playerRigidBody2d.velocity = movementVector.normalized * speed;
+    }
+}
