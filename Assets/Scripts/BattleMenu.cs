a1584204bb7b76b3f4bc817291e137afee35ba5b
mrome007 <mrome007@ucr.edu>
2019-02-04 21:40:18 -0800

    Add intro to battle elements and start to set up battle screen state machine.

--- Assets/Scripts/BattleMenu.cs
+++ Assets/Scripts/BattleMenu.cs
@@ -34,5 +34,6 @@ public enum BattleMenuOptions
     TEXT,
     MOVE,
     ITEM,
-    PKMN
+    PKMN,
+    STATS,
 }
---
+++ Assets/Scripts/BattleStates/BattleState.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleState : MonoBehaviour 
+{
+    public virtual void EnterState()
+    {
+        RegisterEvents();
+    }
+
+    public virtual void ExitState()
+    {
+        UnRegisterEvents();
+    }
+    
+    protected virtual void RegisterEvents()
+    {
+    }
+
+    protected virtual void UnRegisterEvents()
+    {
+    }
+}
