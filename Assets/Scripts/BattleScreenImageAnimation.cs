94202a7cb30245b661e0b9487fc266d22a23ed53
mrome007 <mrome007@ucr.edu>
2019-02-17 18:23:53 -0800

    Add new animations to the battle sprites to handle switching out of pokemon and when enemy trainer loses. Add events in BattleAnimation script so I can handle switching sprites in the future when switching monsters.

--- Assets/Scripts/BattleScreenImageAnimation.cs
+++ Assets/Scripts/BattleScreenImageAnimation.cs
@@ -1,9 +1,14 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BattleScreenImageAnimation : MonoBehaviour 
 {
+    public Action IntoViewComplete;
+    public Action GoComplete;
+    public Action ReturnComplete;
+    
     protected Animator animator;
 
     protected virtual void Awake()
@@ -24,5 +29,39 @@ public class BattleScreenImageAnimation : MonoBehaviour
     public virtual void PlayToView(PocketMonsterParty party)
     {
         animator.Play("View", -1, 0f);
+    }
+
+    public virtual void PlayGo()
+    {
+        animator.Play("Go", -1, 0f);
+    }
+
+    public virtual void PlayReturn()
+    {
+        animator.Play("Return", -1, 0f);
+    }
+
+    protected void PostIntoViewComplete()
+    {
+        if(IntoViewComplete != null)
+        {
+            IntoViewComplete.Invoke();
+        }
+    }
+
+    protected void PostGoComplete()
+    {
+        if(GoComplete != null)
+        {
+            GoComplete.Invoke();
+        }
+    }
+
+    protected void PostReturnComplete()
+    {
+        if(ReturnComplete != null)
+        {
+            ReturnComplete.Invoke();
+        }
     }
 }
