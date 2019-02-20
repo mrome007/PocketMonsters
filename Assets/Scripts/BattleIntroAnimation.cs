9256f2b914964e437e22265fa2dcd6b11be43d16
mrome007 <mrome007@ucr.edu>
2019-02-19 20:16:04 -0800

    Create battle animation class that will be inherited by trainer encounter into and wild encounter intro animation classes.

---
+++ Assets/Scripts/BattleIntroAnimation.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BattleIntroAnimation : MonoBehaviour 
+{
+    [SerializeField]
+    protected BattleScreenImageAnimation playerMonsterAnimation;
+    [SerializeField]
+    protected BattleScreenImageAnimation enemyMonsterAnimation;
+    [SerializeField]
+    protected BattleScreenImageAnimation trainerAnimation;
+    [SerializeField]
+    protected BattleScreenImageAnimation enemyTrainerAnimation;
+
+    protected Animator introBattleAnimator;
+
+    public Action IntroAnimationEnded;
+    public Action GoPlayerAnimationEnded;
+    public Action GoEnemyAnimationEnded;
+
+    public abstract void PlayIntro(PocketMonsterParty player, PocketMonsterParty enemy);
+    public abstract void PlayGoPlayer();
+    public abstract void PlayGoEnemy();
+
+    protected void PostIntroAnimationEnded()
+    {
+        if(IntroAnimationEnded != null)
+        {
+            IntroAnimationEnded.Invoke();
+        }
+    }
+
+    protected void PostGoPlayerAnimationEnded()
+    {
+        if(GoPlayerAnimationEnded != null)
+        {
+            GoPlayerAnimationEnded.Invoke();
+        }
+    }
+
+    protected void PostGoEnemyAnimationEnded()
+    {
+        if(GoEnemyAnimationEnded != null)
+        {
+            GoEnemyAnimationEnded.Invoke();
+        }
+    }
+}
