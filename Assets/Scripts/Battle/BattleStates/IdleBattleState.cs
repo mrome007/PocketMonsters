a1b32b31b3896a47a3322c8230ac95ff6b67107c
mrome007 <mrome007@ucr.edu>
2019-03-15 21:16:30 -0700

    Set up idle battle state. TODO Work on Fight and PKMN buttons

---
+++ Assets/Scripts/Battle/BattleStates/IdleBattleState.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IdleBattleState : BattleState
+{
+    [SerializeField]
+    private BattleMenu menu;
+
+    public override void EnterState(BattleStateArgs battleArgs)
+    {
+        base.EnterState(battleArgs);
+
+        menu.ShowMenuOption(BattleMenuOptions.MAIN, true);
+    }
+}
