f2a38161b13cf1a42056b9a4c102ee7813ef4b00
mrome007 <mrome007@ucr.edu>
2019-03-23 22:17:44 -0700

    Implement icon switcher where it swaps animator controllers.

---
+++ Assets/Scripts/Battle/MonsterIconSwitcher.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterIconSwitcher : MonoBehaviour 
+{
+    private Animator monsterIconAnimator;
+    private int currentIconIndex;
+
+    private void Awake()
+    {
+        monsterIconAnimator = GetComponent<Animator>();
+        currentIconIndex = -1;
+    }
+
+    public void SwitchIcons(int index)
+    {
+        if(currentIconIndex == index)
+        {
+            return;
+        }
+
+        currentIconIndex = index;
+        var bodyType = (BodyType)index;
+        var animatorName = "Ground";
+        switch(bodyType)
+        {
+            case BodyType.BIPEDAL:
+                animatorName = "Ground";
+                break;
+            case BodyType.FLYING:
+                animatorName = "Flying";
+                break;
+            case BodyType.WATER:
+                animatorName = "Water";
+                break;
+            case BodyType.FAIRY:
+                animatorName = "Fairy";
+                break;
+            case BodyType.GRASS:
+                animatorName = "Grass";
+                break;
+            case BodyType.BUG:
+                animatorName = "Bug";
+                break;
+            case BodyType.DRAGON:
+                animatorName = "Dragon";
+                break;
+            case BodyType.QUADRUPED:
+                animatorName = "Normal";
+                break;
+            default:
+                break;
+        }
+
+        monsterIconAnimator.runtimeAnimatorController = (RuntimeAnimatorController)Resources.Load(animatorName);
+    }
+}
--- Assets/Scripts/Monster/PlayerMonsterPKMNScreenStatus.cs
+++ Assets/Scripts/Monster/PlayerMonsterPKMNScreenStatus.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 public class PlayerMonsterPKMNScreenStatus : PlayerMonsterBattleScreenStatus
 {
     [SerializeField]
-    private List<Sprite> pkmnScreenIcons;
+    private MonsterIconSwitcher iconSwitcher;
 
     [SerializeField]
     private Image monsterIconImage;
@@ -14,10 +14,6 @@ public class PlayerMonsterPKMNScreenStatus : PlayerMonsterBattleScreenStatus
     public override void UpdateMonsterStatus(string monsterName, ushort levelNumber, ushort currentHP, ushort monsterHP, int icon = 0)
     {
         base.UpdateMonsterStatus(monsterName, levelNumber, currentHP, monsterHP, icon);
-
-        if(icon >= 0 && icon < pkmnScreenIcons.Count)
-        {
-            monsterIconImage.sprite = pkmnScreenIcons[icon];
-        }
+        iconSwitcher.SwitchIcons(icon);
     }
 }
