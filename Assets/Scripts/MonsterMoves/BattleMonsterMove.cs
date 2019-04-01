529c71573ae0afe95a9971545b1d3a95e4e9a620
mrome007 <mrome007@ucr.edu>
2019-03-31 22:37:21 -0700

    Create battle monster move class. Each party will have one. This will perform the move.

---
+++ Assets/Scripts/MonsterMoves/BattleMonsterMove.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleMonsterMove
+{
+    public bool IsMoveDone { get; private set; }
+
+    private List<MonsterMoveAction> moveActions;
+    private int currentMoveActionIndex;
+
+    public BattleMonsterMove()
+    {
+        IsMoveDone = false;
+        moveActions = new List<MonsterMoveAction>();
+        currentMoveActionIndex = 0;
+    }
+
+    public virtual void PerformBattleMonsterMove()
+    {
+    }
+}
