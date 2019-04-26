b2cbe21b0913b60a722ca8e16554ee4792fcf517
mrome007 <mrome007@ucr.edu>
2019-04-25 22:29:27 -0700

    Create Tackle animation. Also modify placeholder player monsters to be bulbasaur and squirtle since they both have tackle as their starting moves.

---
+++ Assets/Scripts/MonsterMoves/BattleMonsterMoveAnimation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleMonsterMoveAnimation : MonoBehaviour 
+{
+    public Action MonsterMoveAnimationStarted;
+    public Action MonsterMoveAnimationEnded;
+
+    private void PostMonsterMoveAnimationStarted()
+    {
+        if(MonsterMoveAnimationStarted != null)
+        {
+            MonsterMoveAnimationStarted.Invoke();
+        }
+    }
+
+    private void PostMonsterMoveAnimationEnded()
+    {
+        if(MonsterMoveAnimationEnded != null)
+        {
+            MonsterMoveAnimationEnded.Invoke();
+        }
+    }
+}
