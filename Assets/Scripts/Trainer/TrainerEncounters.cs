8b7dd4fb7d891b16845275a2557f3e9a5a2c81b9
mrome007 <mrome007@ucr.edu>
2019-05-04 19:58:37 -0700

    After loading data for trainer encounters, set each battle on the list.

--- Assets/Scripts/Trainer/TrainerEncounters.cs
+++ Assets/Scripts/Trainer/TrainerEncounters.cs
@@ -43,7 +43,15 @@ public class TrainerEncounters : MonoBehaviour, IPersistentData
 
     public void LoadData()
     {
+        BinaryFormatter binaryFormatter = new BinaryFormatter();
+        var folderPath = Path.Combine(Application.persistentDataPath, FolderName);
+        var path = Path.Combine(folderPath, FileName);
+        using(FileStream fileStream = File.Open(path, FileMode.Open))
+        {
+            encounterData = (TrainersEncounteredData)binaryFormatter.Deserialize(fileStream);
+        }
 
+        SetEncounteredBattles();
 
         var handler = LoadComplete;
         if(handler != null)
@@ -125,6 +133,89 @@ public class TrainerEncounters : MonoBehaviour, IPersistentData
             {
                 encounterData.FifteenthTrainerGroup |= mask;
             }
+
+            count++;
+            count %= 32;
+        }
+    }
+
+    private void SetEncounteredBattles()
+    {
+        if(encounterData == null)
+        {
+            return;
+        }
+
+        for(int count= 0, index = 0; index < trainerBattles.Count; index++)
+        {
+            var battle = trainerBattles[index];
+            var mask = 1;
+            mask <<= count;
+            var encountered = 0;
+
+            if(index >= 0 && index < 32)
+            {
+                encountered = encounterData.FirstTrainerGroup & mask;
+            }
+            else if(index >= 32 || index < 64)
+            {
+                encountered = encounterData.SecondTrainerGroup & mask;
+            }
+            else if(index >= 64 || index < 96)
+            {
+                encountered = encounterData.ThirdTrainerGroup & mask;
+            }
+            else if(index >= 96 || index < 128)
+            {
+                encountered = encounterData.FourhtTrainerGroup & mask;
+            }
+            else if(index >= 128 || index < 160)
+            {
+                encountered = encounterData.FifthTrainerGroup & mask;
+            }
+            else if(index >= 160 || index < 192)
+            {
+                encountered = encounterData.SixthTrainerGroup & mask;
+            }
+            else if(index >= 192 || index < 224)
+            {
+                encountered = encounterData.SeventhTrainerGroup & mask;
+            }
+            else if(index >= 224 || index < 256)
+            {
+                encountered = encounterData.EigthTrainerGroup & mask;
+            }
+            else if(index >= 256 || index < 288)
+            {
+                encountered = encounterData.NinthTrainerGroup & mask;
+            }
+            else if(index >= 288 || index < 320)
+            {
+                encountered = encounterData.TenthTrainerGroup & mask;
+            }
+            else if(index >= 320 || index < 352)
+            {
+                encountered = encounterData.EleventhTrainerGroup & mask;
+            }
+            else if(index >= 352 || index < 384)
+            {
+                encountered = encounterData.TwelfthTrainerGroup & mask;
+            }
+            else if(index >= 384 || index < 416)
+            {
+                encountered = encounterData.ThirteenthTrainerGroup & mask;
+            }
+            else if(index >= 416 || index < 448)
+            {
+                encountered = encounterData.FourteenthTrainerGroup & mask;
+            }
+            else if(index >= 448 || index < 480)
+            {
+                encountered = encounterData.FifteenthTrainerGroup & mask;
+            }
+
+            encountered >>= count;
+            battle.Encountered = encountered == 1;
 
             count++;
             count %= 32;
