3853832a3d658d5b9335ef9092ddf412c7fd4274
mrome007 <mrome007@ucr.edu>
2019-05-05 22:26:57 -0700

    TODO Finish save/load operation.

--- Assets/Scripts/Trainer/PlayerMonstersPersistentData.cs
+++ Assets/Scripts/Trainer/PlayerMonstersPersistentData.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 using UnityEngine;
 
 public class PlayerMonstersPersistentData : PersistentData
@@ -13,11 +15,29 @@ public class PlayerMonstersPersistentData : PersistentData
 
     public override void SaveData()
     {
+        var folderPath = Path.Combine(Application.persistentDataPath, FolderName);
+        if(!Directory.Exists(folderPath))
+        {
+            Directory.CreateDirectory(folderPath);
+        }
+
+        var path = Path.Combine(folderPath, FileName);
+        BinaryFormatter binaryFormatter = new BinaryFormatter();
+        using(FileStream fileStream = File.Open(path, FileMode.OpenOrCreate))
+        {
+        }
+
         PostSaveComplete();
     }
 
     public override void LoadData()
     {
+        BinaryFormatter binaryFormatter = new BinaryFormatter();
+        var folderPath = Path.Combine(Application.persistentDataPath, FolderName);
+        var path = Path.Combine(folderPath, FileName);
+        using(FileStream fileStream = File.Open(path, FileMode.Open))
+        {
+        }
         PostLoadComplete();
     }
 }
